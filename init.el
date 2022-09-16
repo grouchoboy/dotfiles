c5a5be2e780c3a1bcd0b6bc663fb335e2a469799
Manu Pascual <manu.pascual.luna@gmail.com>
2022-09-16 12:26:48 +0200

    emacs

--- init.el
+++ init.el
@@ -45,6 +45,13 @@
   (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
   (setq magit-status-buffer-switch-function 'switch-to-buffer))
 
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-mode +1)
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+
 (use-package counsel
   :ensure t
   :config
@@ -74,10 +81,46 @@
   :config
   (which-key-mode))
 
+(use-package slime
+  :ensure t
+  :init
+  (setq inferior-lisp-program "sbcl"))
+
 (use-package lispy
+  :ensure t)
+
+;; Languages
+
+(use-package elixir-mode
+  :ensure t)
+
+(use-package markdown-mode
   :ensure t)
 
 (defun open-init()
   "Open the init file"
   (interactive)
   (find-file "~/.emacs.d/init.el"))
+
+(defun copy-line (arg)
+  "Copy lines (as many as prefix argument) in the kill ring.
+      Ease of use features:
+      - Move to start of next line.
+      - Appends the copy on sequential calls.
+      - Use newline as last char even on the last line of the buffer.
+      - If region is active, copy its lines."
+  (interactive "p")
+  (let ((beg (line-beginning-position))
+        (end (line-end-position arg)))
+    (when mark-active
+      (if (> (point) (mark))
+          (setq beg (save-excursion (goto-char (mark)) (line-beginning-position)))
+        (setq end (save-excursion (goto-char (mark)) (line-end-position)))))
+    (if (eq last-command 'copy-line)
+        (kill-append (buffer-substring beg end) (< end beg))
+      (kill-ring-save beg end)))
+  (kill-append "\n" nil)
+  (beginning-of-line (or (and arg (1+ arg)) 2))
+  (if (and arg (not (= 1 arg))) (message "%d lines copied" )))
+
+(global-set-key "\C-c\C-k" 'copy-line)
