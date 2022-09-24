558a50c6e921aedca1bb2902fe0772b6244f5f90
Manu Pascual <manu.pascual.luna@gmail.com>
2022-09-24 20:34:45 +0200

    emacs remove lispy

--- init.el
+++ init.el
@@ -33,6 +33,7 @@
 (setq-default fill-column 80)
 (setq backup-directory-alist `(("." . ,(expand-file-name "backups" user-emacs-directory))))
 (show-paren-mode t)
+(blink-cursor-mode 0)
 
 (use-package evil
   :ensure t
@@ -94,9 +95,6 @@
   :init
   (setq inferior-lisp-program "sbcl"))
 
-(use-package lispy
-  :ensure t)
-
 ;; Languages
 
 (use-package elixir-mode
@@ -126,26 +124,3 @@
   "Open the init file"
   (interactive)
   (find-file "~/.emacs.d/init.el"))
-
-(defun copy-line (arg)
-  "Copy lines (as many as prefix argument) in the kill ring.
-      Ease of use features:
-      - Move to start of next line.
-      - Appends the copy on sequential calls.
-      - Use newline as last char even on the last line of the buffer.
-      - If region is active, copy its lines."
-  (interactive "p")
-  (let ((beg (line-beginning-position))
-        (end (line-end-position arg)))
-    (when mark-active
-      (if (> (point) (mark))
-          (setq beg (save-excursion (goto-char (mark)) (line-beginning-position)))
-        (setq end (save-excursion (goto-char (mark)) (line-end-position)))))
-    (if (eq last-command 'copy-line)
-        (kill-append (buffer-substring beg end) (< end beg))
-      (kill-ring-save beg end)))
-  (kill-append "\n" nil)
-  (beginning-of-line (or (and arg (1+ arg)) 2))
-  (if (and arg (not (= 1 arg))) (message "%d lines copied" )))
-
-(global-set-key "\C-c\C-k" 'copy-line)
