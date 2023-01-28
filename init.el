b0339df18fa6abffcba96b217b3bbdde44033f0f
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2023-01-28 14:48:55 +0100

    Fedora for the win

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
   (require 'use-package))
 
 (if (eq system-type 'gnu/linux)
-    (set-face-attribute 'default nil :font "JetBrains Mono NL-12"))
+    (set-face-attribute 'default nil :font "JetBrainsMono Nerd Font-12"))
 
 (if (eq system-type 'darwin) 
     (set-face-attribute 'default nil :font "JetBrains Mono Medium-15"))
