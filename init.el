54791830457dbd3f87a369279c2fe6cb677d20a9
Manuel Pascual <manuel.pascual@cabify.com>
2024-04-29 13:04:26 +0200

    emacs font

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
   (require 'use-package))
 
 (if (eq system-type 'gnu/linux)
-    (set-face-attribute 'default nil :font "Fira Mono -10.5"))
+    (set-face-attribute 'default nil :font "JetBrainsMono Nerd Font-10"))
 
 (if (eq system-type 'darwin) 
     (set-face-attribute 'default nil :font "JetBrains Mono-15"))
