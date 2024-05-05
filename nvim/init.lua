f4b4cb1426b9541e22722f04cd8ff6ef2eebec9e
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2024-05-05 20:45:50 +0200

    use frappe

--- nvim/init.lua
+++ nvim/init.lua
@@ -784,11 +784,14 @@ require('lazy').setup({
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {
+      flavour = 'frappe', -- latte, frappe, macchiato, mocha
+    },
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin'
+      vim.cmd.colorscheme 'catppuccin-frappe'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
