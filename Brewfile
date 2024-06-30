8a263af8317d433215c850c22f7157905f7dc318
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2024-06-30 14:19:06 +0200

    remove unused stuff

--- Brewfile
+++
@@ -1,7 +0,0 @@
-
-brew 'git'
-brew 'neovim'
-brew 'zsh'
-brew 'zsh-completions'
-brew 'tmux'
-brew 'fzf'
---
+++ bootstrap/keyboard.sh
@@ -0,0 +1,5 @@
+# https://github.com/zsa/wally/wiki/Linux-install
+#
+sudo dnf -y install gtk3 webkit2gtk3 libusb
+sudo touch /etc/udev/rules.d/50-zsa.rules
+
