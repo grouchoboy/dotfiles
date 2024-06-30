687cbd0d0bc5a41b50356d607e3a5558934ff896
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2024-06-30 13:09:13 +0200

    add docker instructions

---
+++ bootstrap/docker.sh
@@ -0,0 +1,28 @@
+# https://docs.docker.com/engine/install/fedora
+
+sudo dnf remove -y docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+
+sudo dnf -y install dnf-plugins-core
+
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl enable --now docker
+
+sudo groupadd -f docker
+
+sudo usermod -aG docker $USER
+
+newgrp docker
+
+echo "Test the installation with: docker run hello-world"
