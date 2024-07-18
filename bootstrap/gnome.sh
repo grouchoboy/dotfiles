00ea5fd1165407d980465fd7e76793962236c6b0
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2024-07-18 23:04:32 +0200

    welcome back gnome

---
+++ bootstrap/gnome.sh
@@ -0,0 +1,4 @@
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Control><Super>Left']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Control><Super>Right']"
+gsettings set org.gnome.desktop.wm.keybindings close "['<Super>q']"
+gsettings set org.gnome.desktop.wm.keybindings show-desktop "['<Super>d']"
