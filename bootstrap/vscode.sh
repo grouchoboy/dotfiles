8758d41aab5318b0108c4d571416f3d3947269c1
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2024-07-20 13:53:52 +0200

    alacritty no decorations, hello vscode

---
+++ bootstrap/vscode.sh
@@ -0,0 +1,4 @@
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+sudo dnf install -y code
+
