6981c2b26fb568be1bcf0e30e9703a7fb0d1d4da
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2024-07-23 12:33:11 +0200

    add kubectl

--- bootstrap/minikube.sh
+++ bootstrap/minikube.sh
@@ -1,2 +1,9 @@
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm
 sudo rpm -Uvh minikube-latest.x86_64.rpm
+
+cd /tmp
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+mv ./kubectl ~/.local/bin/kubectl
+chmod +x ~/.local/bin/kubectl
+cd -
+
