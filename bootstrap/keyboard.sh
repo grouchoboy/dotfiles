f7264147f08c5afdba079b2acf9bd7eedd372338
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2024-08-03 13:42:24 +0200

    more more

--- bootstrap/keyboard.sh
+++ bootstrap/keyboard.sh
@@ -1,5 +1,5 @@
 # https://github.com/zsa/wally/wiki/Linux-install
 #
-sudo dnf -y install gtk3 webkit2gtk3 libusb
+sudo dnf -y install gtk3 webkit2gtk3 libusb webkit2gtk4.1
 sudo touch /etc/udev/rules.d/50-zsa.rules
 
