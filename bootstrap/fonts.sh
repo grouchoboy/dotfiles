df2ccc8533c157257caa9800c7d9c4be924cf8b8
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2024-08-16 13:32:21 +0200

    add go font

--- bootstrap/fonts.sh
+++ bootstrap/fonts.sh
@@ -1,3 +1,4 @@
+cd /tmp
 # install fira sans
 wget https://github.com/mozilla/Fira/archive/refs/tags/4.202.zip
 unzip 4.202.zip
@@ -11,9 +12,14 @@ unzip JetBrainsMono.zip -d JetBrainsMono
 cp -r JetBrainsMono ~/.local/share/fonts
 rm JetBrainsMono.zip
 
-cd /tmp
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/IBMPlexMono.zip
 unzip IBMPlexMono.zip -d IMBPlexMono
 cp -r IMBPlexMono ~/.local/share/fonts
 rm -rf IBMPlexMono.zip IMBPlexMono
+
+wget https://go.googlesource.com/image/+archive/master/font/gofont/ttfs.tar.gz
+mkdir -p /tmp/go-fonts
+tar -xvzf ttfs.tar.gz -C /tmp/go-fonts/
+cp -r /tmp/go-fonts/ ~/.local/share/fonts
 cd -
+
