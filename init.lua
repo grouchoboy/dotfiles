49bb7422c7efdd32bafd76cefe3be9c8d1d8a2a9
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2025-02-27 14:02:51 +0100

    vim: uncomment autopairs plugin

--- init.lua
+++ init.lua
@@ -982,19 +982,19 @@ require("lazy").setup({
 			--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 		end,
 	},
-	-- {
-	-- 	"windwp/nvim-autopairs",
-	-- 	event = "InsertEnter",
-	-- 	-- Optional dependency
-	-- 	dependencies = { "hrsh7th/nvim-cmp" },
-	-- 	config = function()
-	-- 		require("nvim-autopairs").setup({})
-	-- 		-- If you want to automatically add `(` after selecting a function or method
-	-- 		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-	-- 		local cmp = require("cmp")
-	-- 		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-	-- 	end,
-	-- },
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		-- Optional dependency
+		dependencies = { "hrsh7th/nvim-cmp" },
+		config = function()
+			require("nvim-autopairs").setup({})
+			-- If you want to automatically add `(` after selecting a function or method
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		version = "*",
