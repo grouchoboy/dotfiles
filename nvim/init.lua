5ca7dcf5fadae3df61312327bd0373f668a766b1
Manu Pascual <mpascual@topdoctors.es>
2025-04-04 15:36:14 +0200

    use gruvbox for neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -107,9 +107,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.o.background = 'light'
-colors = require 'custom.plugins.colors'
-colors()
+-- vim.o.background = 'light'
+-- colors = require 'custom.plugins.colors'
+-- colors()
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -760,6 +760,53 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    config = function()
+      local bg = '#ffffea'
+      require('gruvbox').setup {
+        terminal_colors = true, -- add neovim terminal colors
+        undercurl = true,
+        underline = true,
+        bold = false,
+        italic = {
+          strings = false,
+          emphasis = false,
+          comments = false,
+          operators = false,
+          folds = false,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        contrast = 'hard', -- can be "hard", "soft" or empty string
+        palette_overrides = {
+          light0_hard = bg,
+        },
+        overrides = {
+          SignColumn = { bg = bg },
+          LineNr = { bg = bg },
+          CursorLine = { bg = '#f9f5d7' },
+          CursorLineNr = { bg = bg },
+          DiagnosticSignError = { bg = bg },
+          DiagnosticSignWarn = { bg = bg },
+          DiagnosticSignInfo = { bg = bg },
+          DiagnosticSignHint = { bg = bg },
+        },
+        dim_inactive = false,
+        transparent_mode = false,
+      }
+      vim.o.background = 'light'
+      vim.cmd 'colorscheme gruvbox'
+
+      -- local colors = require 'custom.plugins.colors'
+      -- colors()
+    end,
+  },
 
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
@@ -792,7 +839,7 @@ require('lazy').setup({
       require = '🌙',
       source = '📄',
       start = '🚀',
-      task = '📌',
+      taskr = '📌',
       lazy = '💤 ',
     },
   },
