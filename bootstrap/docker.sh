a96cc600c9a1b00ddc58c69bf859291a8740b02c
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2025-04-05 21:04:49 +0200

    update docker instructions to dnf5

--- bootstrap/docker.sh
+++ bootstrap/docker.sh
@@ -13,7 +13,7 @@ sudo dnf remove -y docker \
 
 sudo dnf -y install dnf-plugins-core
 
-sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
 
 sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
