e1a50da8ce29affb4b1bf21c57098bc7750e3fe8
Manu Pascual <mpascual@topdoctors.es>
2025-05-21 15:11:39 +0200

    harpoon

--- nvim/lua/kickstart/plugins/harpoon.lua
+++ nvim/lua/kickstart/plugins/harpoon.lua
@@ -29,7 +29,7 @@ return {
     vim.keymap.set('n', '<leader>l', function()
       harpoon:list():select(3)
     end)
-    vim.keymap.set('n', '<leader>;', function()
+    vim.keymap.set('n', '<leader>p', function()
       harpoon:list():select(4)
     end)
 
