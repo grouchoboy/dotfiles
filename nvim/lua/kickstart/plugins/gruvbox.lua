e731e272670d9b6d8d21111ecbe23a820aa1226f
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2025-06-28 17:56:48 +0200

    nvim: remove custom colors for  gruvbox

--- nvim/lua/kickstart/plugins/gruvbox.lua
+++ nvim/lua/kickstart/plugins/gruvbox.lua
@@ -2,7 +2,6 @@ return {
   'ellisonleao/gruvbox.nvim',
   priority = 1000,
   config = function()
-    local bg = '#ffffea'
     require('gruvbox').setup {
       terminal_colors = true, -- add neovim terminal colors
       undercurl = true,
@@ -22,26 +21,9 @@ return {
       invert_intend_guides = false,
       inverse = true, -- invert background for search, diffs, statuslines and errors
       contrast = 'hard', -- can be "hard", "soft" or empty string
-      palette_overrides = {
-        light0_hard = bg,
-      },
-      overrides = {
-        SignColumn = { bg = bg },
-        LineNr = { bg = bg },
-        CursorLine = { bg = '#f9f5d7' },
-        CursorLineNr = { bg = bg },
-        DiagnosticSignError = { bg = bg },
-        DiagnosticSignWarn = { bg = bg },
-        DiagnosticSignInfo = { bg = bg },
-        DiagnosticSignHint = { bg = bg },
-      },
-      dim_inactive = false,
       transparent_mode = false,
     }
     vim.o.background = 'light'
     vim.cmd 'colorscheme gruvbox'
-
-    -- local colors = require 'custom.plugins.colors'
-    -- colors()
   end,
 }
