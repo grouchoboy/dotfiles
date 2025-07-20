864150d263fb6570631b30c4be6ac207c341b7cb
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2025-07-20 14:27:10 +0200

    (arch): add wl-clipboard

--- bootstrap/arch.sh
+++ bootstrap/arch.sh
@@ -1,5 +1,5 @@
 sudo pacman -S zellij alacritty zsh nvim fzf inotify-tools podman docker unzip inetutils distrobox \\
-    vlc okular foliate mise
+    vlc okular foliate mise wl-clipboard
 
 yay -S 1password
 
