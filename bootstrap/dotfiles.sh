83afa570fb3e7ba1a97616fc2c28bbd0835bd5d7
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2025-08-10 14:04:47 +0200

    (hypr): add config

--- bootstrap/dotfiles.sh
+++ bootstrap/dotfiles.sh
@@ -1,6 +1,9 @@
 cd ~
 git clone git@github.com:grouchoboy/dotfiles.git
 
+ln -s /home/manu/dotfiles/hypr /home/manu/.config/hypr
+ln -s /home/manu/dotfiles/waybar /home/manu/.config/waybar
+
 ln -s /home/manu/dotfiles/nvim /home/manu/.config/nvim
 # mkdir /home/manu/.config/nvim
 # ln -s /home/manu/dotfiles/init.lua /home/manu/.config/nvim/init.lua
@@ -17,7 +20,6 @@ ln -s /home/manu/dotfiles/lazygit /home/manu/.config/lazygit
 
 ln -s /home/manu/dotfiles/sway /home/manu/.config/sway
 ln -s /home/manu/dotfiles/swaylock /home/manu/.config/swaylock
-ln -s /home/manu/dotfiles/waybar /home/manu/.config/waybar
 ln -s /home/manu/dotfiles/rofi /home/manu/.config/rofi
 
 ln -s $HOME/dotfiles/bat $HOME/.config/bat
