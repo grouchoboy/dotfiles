303ea6a5231873f345afd38f5e257afe4958e91d
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2025-08-10 13:56:14 +0200

    add arch folder to bootstrap

---
+++ bootstrap/arch/elixir.sh
@@ -0,0 +1,6 @@
+# https://github.com/asdf-vm/asdf-erlang
+
+sudo pacman -S --needed base-devel ncurses glu mesa wxwidgets-gtk3 libpng libssh unixodbc libxslt fop
+
+# mise use -g erlang@28.0.2
+# mise use -g elixir@1.18.4
