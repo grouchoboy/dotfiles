f8295fbf0e73759b1b95ac000a927725caf97f18
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2025-08-10 14:27:45 +0200

    (hypr): screenshoot

--- bootstrap/arch/arch.sh
+++ bootstrap/arch/arch.sh
@@ -1,5 +1,8 @@
 sudo pacman -S zellij alacritty zsh nvim fzf inotify-tools podman docker unzip inetutils distrobox \\
-    vlc okular foliate mise wl-clipboard
+    vlc okular foliate mise wl-clipboard swappy
 
 yay -S 1password
+ 
+# codecs
+sudo pacman -S gst-plugins-base gst-plugins-good gst-plugins-ugly gst-libav ffmpeg vlc vlc-plugins-all
 
