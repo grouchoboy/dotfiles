2b58e21a20de3f3e62d44bd030c237e22bce1fff
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2025-08-15 20:39:15 +0200

    more

--- bootstrap/arch/arch.sh
+++ bootstrap/arch/arch.sh
@@ -10,7 +10,7 @@ sudo pacman -S gst-plugins-base gst-plugins-good gst-plugins-ugly gst-libav ffmp
 
 sudo pacman -S bluez bluez-utils blueman
 
-sudo pacman -S lxappearance qt6ct hyprpaper hyprpolkitagent hyprsunset hyprlock papirus-icon-theme cliphist lf hyprpicker nwg-look
+sudo pacman -S lxappearance qt6ct hyprpaper hyprpolkitagent hyprsunset hyprlock papirus-icon-theme cliphist lf hyprpicker nwg-look fwupd
 # systemctl --user enable --now hyprpolkitagent.service
 # systemctl --user enable --now hyprsunset.service
 
