fca9f4712156c41ee383172cbe696f912802b73e
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2025-08-23 21:57:19 +0200

    back to gruvbox hard

--- nvim/lua/kickstart/plugins/gruvbox.lua
+++ nvim/lua/kickstart/plugins/gruvbox.lua
@@ -20,7 +20,7 @@ return {
       invert_tabline = false,
       invert_intend_guides = false,
       inverse = true, -- invert background for search, diffs, statuslines and errors
-      contrast = 'soft', -- can be "hard", "soft" or empty string
+      contrast = 'hard', -- can be "hard", "soft" or empty string
       transparent_mode = false,
     }
     vim.o.background = 'light'
