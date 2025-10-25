edd94c3c5d93baeada4b58ab07c32ad39480f8c2
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2025-10-25 14:06:45 +0200

    (nvim): try to remove vim config warnigns
    didn't work

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -306,11 +306,15 @@ require('lazy').setup({
           -- capabilities = {},
           settings = {
             Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
+
+              -- completion = {
+              --   callSnippet = 'Replace',
+              -- },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = {
+                disable = { 'missing-fields' },
+                globals = { 'vim' },
+              },
             },
           },
         },
