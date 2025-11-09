e7ac8a916069a1d455c18b6daeac3b50dc29e60f
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2025-11-09 15:23:51 +0100

    (git): config pull.rebase true

--- bootstrap/git.sh
+++ bootstrap/git.sh
@@ -3,4 +3,5 @@
 git config --global user.email "manu.pascual.luna@gmail.com"
 git config --global user.name "Manu Pascual Luna"
 git config --global push.autoSetupRemote true
+git config --global pull.rebase true
 
