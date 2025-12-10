0d42a099b3fb6cfbfc9db017291803db6611a885
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2025-12-10 22:32:55 +0100

    (opensuse): use docker without sudo

--- bootstrap/opensuse/docker.sh
+++ bootstrap/opensuse/docker.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 sudo zypper install docker docker-compose docker-buildx
+
+sudo systemctl enable --now docker.socket
+# sudo usermod -G docker -a $USER
+# newgrp docker
+# sudo systemctl restart docker
