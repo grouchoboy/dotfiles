faaf6a1fcd69cc6d2e1009eda22f2bdd05b4fc2a
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2025-12-11 15:12:20 +0100

    (kinoite): fix libvirt packages

--- bootstrap/virt-kinoite.sh
+++ bootstrap/virt-kinoite.sh
@@ -2,5 +2,5 @@
 
 sudo rpm-ostree upgrade
 systemctl reboot
-sudo rpm-ostree install qemu virt-manager libvirt virt-viewer libvirt-daemon-config guestfs-tools libguestfs-tools python3-libguestfs virt-top
+sudo rpm-ostree install qemu-kvm virt-manager virt-install libvirt virt-viewer libvirt-daemon-config-network  libvirt-daemon-kvm guestfs-tools python3-libguestfs virt-top
 systemctl reboot
