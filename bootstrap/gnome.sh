316f5351ed09fbd4d6b5dae887748007ea460f24
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2025-12-21 20:44:37 +0100

    (gnome): add shortcuts

--- bootstrap/gnome.sh
+++ bootstrap/gnome.sh
@@ -1,4 +1,6 @@
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Control><Super>Left']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Control><Super>Right']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Control><Alt><Super>Left']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Control><Alt><Super>Right']"
 gsettings set org.gnome.desktop.wm.keybindings close "['<Super>q']"
 gsettings set org.gnome.desktop.wm.keybindings show-desktop "['<Super>d']"
