769b4d3c56dc242b6527f97a9b14c730d606887f
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2026-01-24 22:58:55 +0100

    (flutter): add flutter

---
+++ bootstrap/android.sh
@@ -0,0 +1,3 @@
+sudo dnf install zlib-ng-compat.i686 ncurses-compat-libs.i686 bzip2-libs.i686 \
+libstdc++.i686 libX11.i686 libXrender.i686 libXrandr.i686 mesa-libGL.i686 \
+glibc.i686 libxcrypt.i686
