cb2b9b9ef50016ed9520ff9be23094e154cae566
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2026-03-24 13:40:12 +0100

    debian

--- bootstrap/debian/apps.sh
+++ bootstrap/debian/apps.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo apt install stow alacritty zsh tmux fzf direnv kwin-addons wget vlc flatpak plasma-discover-backend-flatpak jq fastfetch
+sudo apt install stow alacritty zsh tmux fzf direnv kwin-addons wget vlc flatpak plasma-discover-backend-flatpak jq fastfetch transmission-qt
