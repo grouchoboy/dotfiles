c31fa7674c62c13eda2aa09b5b02d69602a5c42c
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2026-03-31 11:00:14 +0200

    iptables rules for libvirt

--- bootstrap/virt.sh
+++ bootstrap/virt.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
 sudo dnf group install --with-optional virtualization
 sudo systemctl enable --now libvirtd
-sudo usermod -a -G libvirt manu
+sudo usermod -a -G libvirt $USER
 newgrp libvirt
+
+# sudo iptables -S FORWARD
+# sudo iptables -P FORWARD ACCEPT
+
