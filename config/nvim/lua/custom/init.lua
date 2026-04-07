43dc1f56225a5f6b689d573e19975633b5f97746
Manu Pascual Luna <manu.pascual.luna@gmail.com>
2026-04-07 15:02:03 +0200

    nvim: add undotree

--- config/nvim/lua/custom/init.lua
+++ config/nvim/lua/custom/init.lua
@@ -7,7 +7,7 @@ require("custom.conform")()
 require("custom.lspconfig")()
 require("custom.blink")()
 require("custom.treesitter")()
-require("lualine").setup()
+require("lualine").setup({})
 require("custom.autocmd")
 require("fidget").setup({
 	notification = {
@@ -17,3 +17,6 @@ require("fidget").setup({
 
 vim.keymap.set("n", "<leader>qe", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>qq", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+vim.cmd.packadd("nvim.undotree")
+vim.keymap.set("n", "<leader>qo", require("undotree").open, { desc = "Open undotree" })
